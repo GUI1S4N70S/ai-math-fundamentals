7df51c10b2014971e7e268415fac1cf1e48b893f
Guilherme Santos - ABF <oficial.guilhermesantos@hotmail.com>
2026-08-27 13:03:28 -0300

    PARTE 2: estudos de Cálculo/gradiente: IA aprendendo com os próprios erros

--- gradiente.py
+++ gradiente.py
@@ -1,7 +1,7 @@
 # aprendizado: Gradient Descent (Descida do Gradiente)
 
-# 1. Definimos nosso erro inicial. 
-# (ustudo/suposição): a IA começou chutando um valor ruim, muito alto
+# 1. Definiu erro inicial. 
+# (ustudo/suposição: a IA começou chutando um valor ruim, muito alto
 peso_atual = 5.0  
 taxa_aprendizado = 0.1  #tamanho do passo que a IA dá a cada tentativa (learning Rate)
 epocas = 10  # quantidade de vezes que a IA vai tentar aprender com o erro
